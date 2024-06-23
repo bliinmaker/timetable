03e18589cf5c0297ebd62f369313ef7818f9c5cd
bliinmaker <kir05062005@gmail.com>
2024-06-24 01:00:00 +0300

    fix: pylint

--- tests/test.sh
+++ tests/test.sh
@@ -3,4 +3,5 @@ export PG_PORT=5432
 export PG_USER=test
 export PG_PASSWORD=test
 export PG_DBNAME=postgres
+export DJANGO_SECRET_KEY="django-insecure-=jj2j3&j!13etr9)&y018#*^-uv1$a70(2%52*rgco!btl7#@s"
 python3 manage.py test $1
--- timetable/settings.py
+++ timetable/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=jj2j3&j!13etr9)&y018#*^-uv1$a70(2%52*rgco!btl7#@s"
+SECRET_KEY = getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
